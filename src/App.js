83b0aba0e8ce7f482b84b665449c04e9db918316
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-12-15 22:45:36 +0000

    file setup - first layer of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import {Component} from 'react'
+import Deals from './Deals.js'
+import Login from './Login.js'
+import NewProvider from './NewProvider.js'
+import Providers from './Providers.js'
+import Register from './Register.js'
+import ShowDeal from './ShowDeal.js'
+import ShowProvider from './ShowProvider.js'
+import ShowUser from './ShowUser.js'
 import {
   BrowserRouter as Router,
   Route,
@@ -82,8 +90,14 @@ const BasicExample = () => (
       <hr/>
 
       <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
+      <Route exact path="/providers" component={Providers}/>
+      <Route path="/providers/new" component={NewProvider}/>
+      <Route path="/provider/:id" component={ShowProvider}/>
+      <Route path="/deals" component={Deals}/>
+      <Route path="/deal/:id" component={ShowDeal}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/user/:id" component={ShowUser}/>
+      <Route path="/login" component={Login}/>
     </div>
   </Router>
 )
