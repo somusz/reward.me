1fcbf7e4573c9d16c4d7dc72e8a4a74b5bae7826
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-12-20 21:25:36 +0000

    registration fixed

--- back-server/routes.js
+++ back-server/routes.js
@@ -15,8 +15,8 @@ module.exports = (knex) => {
       .from('users')
       .where('email', req.body.email)
       .then( (result) => {
-        // if (bcrypt.compareSync(req.body.password, result[0])) {
-        if (req.body.password === result[0].password_digest) {
+        if (bcrypt.compareSync(req.body.password, result[0].password_digest)) {
+        // if (req.body.password === result[0].password_digest) {
 
           console.log('Login Successful')
 
